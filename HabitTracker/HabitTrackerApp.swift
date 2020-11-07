38b9b92656adf4d03e5ef8b15a35a54f2452cb23
antonivegaochoa <tonivegaochoa@gmail.com>
2020-11-06 19:44:06 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Antonio Vega on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
